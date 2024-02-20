20fa4a4c50c77c33231c63e4662d1a5dae347a62
andreipu <andrei.purice1203@gmail.com>
2024-02-20 19:15:44 +0200

    first commit

---
+++ src/test/java/com/pdfreader/pdfreader/PdfreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdfreader.pdfreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
